60cf0ae4846dda889c01d64d581474e3a3f2bd14
Ashwin Sreedhar <s223955271@deakin.edu.au>
2024-09-13 09:06:13 +1000

    Updating for docker

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,10 +2,4 @@ set -u # or set -o nounset
 : "$REGISTRY_HOSTNAME"
 : "$VERSION"
 
-cd ./book_catalog
-
-docker build --no-cache -t $REGISTRY_HOSTNAME/catalog:$VERSION .
-
-cd ./inventory_management
-
-docker build --no-cache -t $REGISTRY_HOSTNAME/inventory:$VERSION .
+./docker-compose up -d --build
